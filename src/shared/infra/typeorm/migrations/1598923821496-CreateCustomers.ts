3f54788b7f513bb2fea024ef7d7c28db2b03ae0a
Italox <italoaurelior@gmail.com>
2020-08-31 22:31:18 -0300

    :construction: Start challenge, migrations + containers

---
+++ src/shared/infra/typeorm/migrations/1598923821496-CreateCustomers.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateCustomers1598923821496
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
